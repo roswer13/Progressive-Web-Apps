dda128b78d32fbfbad2aa26982a6f67319be5847
Roswer García <roswer13@gmail.com>
2024-10-17 11:48:01 +0200

    feat: caching

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -1,6 +1,7 @@
 var shareImageButton = document.querySelector('#share-image-button');
 var createPostArea = document.querySelector('#create-post');
 var closeCreatePostModalButton = document.querySelector('#close-create-post-modal-btn');
+var sharedMomentsArea = document.querySelector('#shared-moments');
 
 function openCreatePostModal() {
   createPostArea.style.display = 'block';
@@ -20,6 +21,17 @@ function openCreatePostModal() {
 
     deferredPrompt = null;
   }
+
+  /*
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations()
+      .then(function (registrations) {
+        for (var i = 0; i < registrations.length; i++) {
+          registrations[i].unregister();
+        }
+      });
+  }
+ */
 }
 
 function closeCreatePostModal() {
@@ -29,6 +41,25 @@ function closeCreatePostModal() {
 shareImageButton.addEventListener('click', openCreatePostModal);
 
 closeCreatePostModalButton.addEventListener('click', closeCreatePostModal);
+
+// Currently not in use, allows to save assets in cache on demand otherwise
+function onSaveButtonClicked(event) {
+  console.log('clicked');
+
+  if ('caches' in window) {
+    caches.open('user-requested')
+      .then(function (cache) {
+        cache.add('https://httpbin.org/get');
+        cache.add('/src/images/sf-boat.jpg');
+      });
+  }
+}
+
+function clearCards() {
+  while (sharedMomentsArea.hasChildNodes()) {
+    sharedMomentsArea.removeChild(sharedMomentsArea.lastChild);
+  }
+}
 
 function createCard() {
   var cardWrapper = document.createElement('div');
@@ -40,7 +71,7 @@ function createCard() {
   cardTitle.style.height = '180px';
   cardWrapper.appendChild(cardTitle);
   var cardTitleTextElement = document.createElement('h2');
-  cardSupportingText.style.color = 'white';
+  cardTitleTextElement.style.color = 'white';
   cardTitleTextElement.className = 'mdl-card__title-text';
   cardTitleTextElement.textContent = 'San Francisco Trip';
   cardTitle.appendChild(cardTitleTextElement);
@@ -48,15 +79,50 @@ function createCard() {
   cardSupportingText.className = 'mdl-card__supporting-text';
   cardSupportingText.textContent = 'In San Francisco';
   cardSupportingText.style.textAlign = 'center';
+
+  var cardSaveButton = document.createElement('button');
+  cardSaveButton.textContent = 'Save';
+  cardSaveButton.addEventListener('click', onSaveButtonClicked);
+  cardSupportingText.appendChild(cardSaveButton);
+
   cardWrapper.appendChild(cardSupportingText);
-  componentHandler.upgradeElement(cardWrapper);
+  // componentHandler.upgradeElement(cardWrapper);
   sharedMomentsArea.appendChild(cardWrapper);
 }
 
-fetch('https://httpbin.org/get')
+var url = 'https://httpbin.org/post';
+var networkDataReceived = false;
+
+fetch(url, {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  },
+  body: JSON.stringify({ message: 'Some message' })
+})
   .then(function (res) {
     return res.json();
   })
   .then(function (data) {
+    networkDataReceived = true;
+    console.log('From web', data);
+    clearCards();
     createCard();
   });
+
+if ('caches' in window) {
+  caches.match(url)
+    .then(function (response) {
+      if (response) {
+        return response.json();
+      }
+    })
+    .then(function (data) {
+      console.log('From cache', data);
+      if (!networkDataReceived) {
+        clearCards();
+        createCard();
+      }
+    });
+}
--- public/sw.js
+++ public/sw.js
@@ -1,26 +1,43 @@
-var CACHE_STATIC_NAME = 'static-v2';
-var CACHE_DYNAMIC_NAME = 'dynamic-v1';
+var CACHE_STATIC_NAME = 'static-v13';
+var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+var STATIC_FILES = [
+    '/',
+    '/index.html',
+    '/offline.html',
+    '/src/js/app.js',
+    '/src/js/feed.js',
+    '/src/js/promise.js',
+    '/src/js/fetch.js',
+    '/src/js/material.min.js',
+    '/src/css/app.css',
+    '/src/css/feed.css',
+    '/src/images/main-image.jpg',
+    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+];
+
+/*
+function trimCache(cacheName, maxItems) {
+    caches.open(cacheName)
+        .then(function (cache) {
+            return cache.keys()
+                .then(function (keys) {
+                    if (keys.length > maxItems) {
+                        cache.delete(keys[0])
+                            .then(trimCache(cacheName, maxItems));
+                    }
+                });
+        });
+}
+*/
 
 self.addEventListener('install', function (event) {
     console.log('[Service Worker] Installing Service Worker ...', event);
     event.waitUntil(caches.open(CACHE_STATIC_NAME)
         .then(function (cache) {
             console.log('[Service Worker] Precaching App Shell');
-            cache.addAll([
-                '/',
-                '/index.html',
-                '/src/js/app.js',
-                '/src/js/feed.js',
-                '/src/js/promise.js',
-                '/src/js/fetch.js',
-                '/src/js/material.min.js',
-                '/src/css/app.css',
-                '/src/css/feed.css',
-                '/src/images/main-image.jpg',
-                'https://fonts.googleapis.com/css?family=Roboto:400,700',
-                'https://fonts.googleapis.com/icon?family=Material+Icons',
-                'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
-            ]);
+            cache.addAll(STATIC_FILES);
         }));
 });
 
@@ -40,6 +57,66 @@ self.addEventListener('activate', function (event) {
     return self.clients.claim();
 });
 
+function isInArray(string, array) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === string) {
+            return true;
+        }
+    }
+    return false;
+}
+
+self.addEventListener('fetch', function (event) {
+    var url = 'https://httpbin.org/get';
+    if (event.request.url.indexOf(url) > -1) {
+        event.respondWith(
+            caches.open(CACHE_DYNAMIC_NAME)
+                .then(function (cache) {
+                    return fetch(event.request)
+                        .then(function (res) {
+                            // trimCache(CACHE_DYNAMIC_NAME, 3);
+                            cache.put(event.request, res.clone());
+                            return res;
+                        });
+                })
+        );
+    }
+    else if (isInArray(event.request.url, STATIC_FILES)) {
+        event.respondWith(
+            caches.match(event.request)
+        );
+    }
+    else {
+        event.respondWith(
+            caches.match(event.request)
+                .then(function (response) {
+                    if (response) {
+                        return response;
+                    } else {
+                        return fetch(event.request)
+                            .then(function (res) {
+                                return caches.open(CACHE_DYNAMIC_NAME)
+                                    .then(function (cache) {
+                                        // trimCache(CACHE_DYNAMIC_NAME, 3);
+                                        cache.put(event.request.url, res.clone());
+                                        return res;
+                                    });
+                            })
+                            .catch(function (err) {
+                                return caches.open(CACHE_STATIC_NAME)
+                                    .then(function (cache) {
+                                        if (event.request.headers.get('accept').includes('text/html')) {
+                                            return cache.match('/offline.html');
+                                        }
+                                    });
+                            });
+                    }
+                })
+        );
+    }
+});
+
+/*
 self.addEventListener('fetch', function (event) {
     event.respondWith(
         caches.match(event.request).then(function (response) {
@@ -55,7 +132,7 @@ self.addEventListener('fetch', function (event) {
                             });
                     })
                     .catch(function (err) {
-                        return caches.open('static')
+                        return caches.open(CACHE_STATIC_NAME)
                             .then(function (cache) {
                                 return cache.match('/offline.html');
                             })
@@ -64,3 +141,31 @@ self.addEventListener('fetch', function (event) {
         })
     );
 });
+*/
+
+// Caahe-only
+/*
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        fetch(event.request)
+    );
+});
+*/
+
+/*
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        fetch(event.request)
+            .then(function (res) {
+                return caches.open(CACHE_DYNAMIC_NAME)
+                    .then(function (cache) {
+                        cache.put(event.request.url, res.clone());
+                        return res;
+                    });
+            })
+            .catch(function (err) {
+                return caches.match(event.request)
+            })
+    );
+});
+*/
